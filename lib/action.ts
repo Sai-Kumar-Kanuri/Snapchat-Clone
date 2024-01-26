1c4f32b51354f098909a6d4347c57659567a399f
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2024-01-26 22:34:45 +0530

    signup and login done

---
+++ lib/action.ts
@@ -0,0 +1,18 @@
+"use server";
+import { signIn, signOut } from "@/auth";
+
+export async function authAction() {
+    try {
+        await signIn("github"); // redirect()
+    } catch (error: any) {
+        if (error.message === "NEXT_REDIRECT") {
+            throw error;
+        }
+        return error.message;
+    }
+}
+
+export async function logoutAction() {
+    "use server"
+    await signOut();
+}
